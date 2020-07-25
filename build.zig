39c7fb8216442e3ae94b36fb2fb2cabda3a4ada8
Lee Cannon <leecannon@leecannon.xyz>
2020-07-25 15:52:08 +0100

    Initial

---
+++ build.zig
@@ -0,0 +1,14 @@
+const Builder = @import("std").build.Builder;
+
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+    const lib = b.addStaticLibrary("zig-x86_64", "src/index.zig");
+    lib.setBuildMode(mode);
+    lib.install();
+
+    var main_tests = b.addTest("src/index.zig");
+    main_tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&main_tests.step);
+}
