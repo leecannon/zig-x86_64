7847329dec654318e909cda1f44ef2008611fcc4
Lee Cannon <leecannon@leecannon.xyz>
2020-07-29 21:53:21 +0100

    Implemented RdRand and also added RdSeed

--- src/instructions/random.zig
+++ src/instructions/random.zig
@@ -16,22 +16,81 @@ pub const RdRand = struct {
     /// Uniformly sampled u64.
     /// May fail in rare circumstances or heavy load.
     pub inline fn get_u64(self: RdRand) ?u64 {
-        // TODO: Investigate implementing this
-        @compileError("This requires an intrinsic that zig does not provide `_rdrand64_step`");
+        var carry: u8 = undefined;
+        const num: u64 = asm volatile ("rdrand %[result]; setc %[carry]"
+            : [result] "=r" (-> u64)
+            : [carry] "qm" (&carry)
+        );
+        return if (carry == 0) null else num;
     }
 
     /// Uniformly sampled u32.
     /// May fail in rare circumstances or heavy load.
     pub inline fn get_u32(self: RdRand) ?u32 {
-        // TODO: Investigate implementing this
-        @compileError("This requires an intrinsic that zig does not provide `_rdrand32_step`");
+        var carry: u8 = undefined;
+        const num: u32 = asm volatile ("rdrand %[result]; setc %[carry]"
+            : [result] "=r" (-> u32)
+            : [carry] "qm" (&carry)
+        );
+        return if (carry == 0) null else num;
     }
 
     /// Uniformly sampled u16.
     /// May fail in rare circumstances or heavy load.
     pub inline fn get_u16(self: RdRand) ?u16 {
-        // TODO: Investigate implementing this
-        @compileError("This requires an intrinsic that zig does not provide `_rdrand16_step`");
+        var carry: u8 = undefined;
+        const num: u16 = asm volatile ("rdrand %[result]; setc %[carry]"
+            : [result] "=r" (-> u16)
+            : [carry] "qm" (&carry)
+        );
+        return if (carry == 0) null else num;
+    }
+};
+
+/// Used to obtain seed numbers using x86_64's RDSEED opcode
+pub const RdSeed = struct {
+    /// Creates RdSeed if RDSEED is supported
+    pub inline fn new() ?RdSeed {
+        // RDSEED support indicated by CPUID page 07h, ebx bit 18
+        // https://en.wikipedia.org/wiki/RdRand#Overview
+        const cpu_id = cpuid(0x7);
+        if (cpu_id.ebx & (1 << 18) != 0) {
+            return RdSeed{};
+        }
+        return null;
+    }
+
+    /// Random u64 seed directly from entropy store.
+    /// May fail in rare circumstances or heavy load.
+    pub inline fn get_u64(self: RdSeed) ?u64 {
+        var carry: u8 = undefined;
+        const num: u64 = asm volatile ("rdseed %[result]; setc %[carry]"
+            : [result] "=r" (-> u64)
+            : [carry] "qm" (&carry)
+        );
+        return if (carry == 0) null else num;
+    }
+
+    /// Random u32 seed directly from entropy store.
+    /// May fail in rare circumstances or heavy load.
+    pub inline fn get_u32(self: RdSeed) ?u32 {
+        var carry: u8 = undefined;
+        const num: u32 = asm volatile ("rdseed %[result]; setc %[carry]"
+            : [result] "=r" (-> u32)
+            : [carry] "qm" (&carry)
+        );
+        return if (carry == 0) null else num;
+    }
+
+    /// Random u16 seed directly from entropy store.
+    /// May fail in rare circumstances or heavy load.
+    pub inline fn get_u16(self: RdSeed) ?u16 {
+        var carry: u8 = undefined;
+        const num: u16 = asm volatile ("rdseed %[result]; setc %[carry]"
+            : [result] "=r" (-> u16)
+            : [carry] "qm" (&carry)
+        );
+        return if (carry == 0) null else num;
     }
 };
 
