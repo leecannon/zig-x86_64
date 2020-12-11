0ec46c9e93f69959e2db5d80eb7c747676712da0
Lee Cannon <leecannon@leecannon.xyz>
2020-12-11 17:12:41 +0000

    Cleaned up lock

--- src/additional/lock.zig
+++ src/additional/lock.zig
@@ -2,71 +2,98 @@ usingnamespace @import("../common.zig");
 
 /// A spinlock that disables interrupts (if they are enabled) when the lock is taken
 /// and enables them again when it is released if they were previously enabled.
-/// ### Remarks:
-/// If the lock is not acquired immediately then a weak spin on the lock bit occurs with interrupts enabled
 pub const KernelSpinLock = struct {
-    locked: bool,
+    const Self = @This();
 
-    /// A token representing a locked lock
-    pub const LockToken = struct {
-        /// Were interrupts enabled prior to the lock being acquired
+    state: State,
+
+    const State = enum(u8) {
+        Unlocked,
+        Locked,
+    };
+
+    pub const Held = struct {
         interrupts_enabled: bool,
-        lock: *KernelSpinLock,
+        spinlock: *Self,
 
-        // Unlocks the lock
-        pub inline fn unlock(self: *const LockToken) void {
-            @atomicStore(bool, &self.lock.locked, false, .Release);
+        pub fn release(self: Held) void {
+            @atomicStore(State, &self.lock.locked, .Unlocked, .Release);
             if (self.interrupts_enabled) instructions.interrupts.enable();
         }
     };
 
-    /// Create a new SpinLock
-    pub inline fn init() KernelSpinLock {
-        return KernelSpinLock{ .locked = false };
+    pub inline fn init() Self {
+        return .{ .state = .Unlocked };
     }
 
-    /// Try to acquire the lock, returns a LockToken if acquired null otherwise
-    /// Interrupts are disabled while the lock is held and re-enabled upon release if previously enabled.
-    pub fn tryLock(self: *KernelSpinLock) ?LockToken {
+    pub fn tryAcquire(self: *Self) ?Held {
         const interrupts_enabled = instructions.interrupts.areEnabled();
 
         if (interrupts_enabled) instructions.interrupts.disable();
 
-        if (@cmpxchgStrong(bool, &self.locked, false, true, .Acquire, .Acquire) == null) {
-            return LockToken{
-                .interrupts_enabled = interrupts_enabled,
-                .lock = self,
+        switch (@atomicRmw(State, &self.state, .Xchg, .Locked, .Acquire)) {
+            .Unlocked => {
+                return Held{
+                    .interrupts_enabled = interrupts_enabled,
+                    .spinlock = self,
+                };
+            },
+            .Locked => {
+                if (interrupts_enabled) instructions.interrupts.enable();
+                return null;
+            },
+        }
+    }
+
+    pub fn acquire(self: *Self) Held {
+        while (true) {
+            return self.tryAcquire() orelse {
+                instructions.pause();
+                continue;
             };
         }
-
-        if (interrupts_enabled) instructions.interrupts.enable();
-        return null;
     }
 
-    /// Acquire the lock.
-    /// Interrupts are disabled while the lock is held and re-enabled upon release if previously enabled.
-    /// If the lock is not acquired immediately then a weak spin on the lock bit occurs with interrupts enabled
-    pub fn lock(self: *KernelSpinLock) LockToken {
-        const interrupts_enabled = instructions.interrupts.areEnabled();
+    test "" {
+        std.testing.refAllDecls(@This());
+    }
+};
 
-        if (interrupts_enabled) instructions.interrupts.disable();
+pub const KernelSpinLockNoDisableInterrupts = struct {
+    const Self = @This();
 
+    state: State,
+
+    const State = enum(u8) {
+        Unlocked,
+        Locked,
+    };
+
+    pub const Held = struct {
+        spinlock: *Self,
+
+        pub inline fn release(self: Held) void {
+            @atomicStore(State, &self.lock.locked, .Unlocked, .Release);
+        }
+    };
+
+    pub inline fn init() Self {
+        return .{ .state = .Unlocked };
+    }
+
+    pub fn tryAcquire(self: *Self) ?Held {
+        return switch (@atomicRmw(State, &self.state, .Xchg, .Locked, .Acquire)) {
+            .Unlocked => Held{ .spinlock = self },
+            .Locked => null,
+        };
+    }
+
+    pub fn acquire(self: *Self) Held {
         while (true) {
-            if (@cmpxchgStrong(bool, &self.locked, false, true, .Acquire, .Acquire) == null) {
-                return LockToken{
-                    .interrupts_enabled = interrupts_enabled,
-                    .lock = self,
-                };
-            }
-
-            if (interrupts_enabled) instructions.interrupts.enable();
-
-            instructions.pause();
-            // while (@atomicLoad(bool, &self.locked, .Acquire)) {
-            //     instructions.pause();
-            // }
-
-            if (interrupts_enabled) instructions.interrupts.disable();
+            return self.tryAcquire() orelse {
+                instructions.pause();
+                continue;
+            };
         }
     }
 
