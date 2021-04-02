38c18231802ab03444c09f23e8fd1e23e697613f
Lee Cannon <leecannon@leecannon.xyz>
2021-04-03 00:59:39 +0100

    non generic functions are easier to use in zig

--- src/structures/paging/frame_alloc.zig
+++ src/structures/paging/frame_alloc.zig
@@ -10,6 +10,30 @@ pub const FrameAllocator = struct {
     z_impl_deallocateFrame2MiB: fn (frameAllocator: *FrameAllocator, frame: paging.PhysFrame2MiB) void,
     z_impl_deallocateFrame1GiB: fn (frameAllocator: *FrameAllocator, frame: paging.PhysFrame1GiB) void,
 
+    pub fn allocate4KiB(frameAllocator: *FrameAllocator) callconv(.Inline) ?paging.PhysFrame {
+        return frameAllocator.allocateFrame(.Size4KiB);
+    }
+
+    pub fn allocate2MiB(frameAllocator: *FrameAllocator) callconv(.Inline) ?paging.PhysFrame2MiB {
+        return frameAllocator.allocateFrame(.Size2MiB);
+    }
+
+    pub fn allocate1GiB(frameAllocator: *FrameAllocator) callconv(.Inline) ?paging.PhysFrame1GiB {
+        return frameAllocator.allocateFrame(.Size1GiB);
+    }
+
+    pub fn deallocate4KiB(frameAllocator: *FrameAllocator, frame: paging.PhysFrame) callconv(.Inline) void {
+        return frameAllocator.deallocateFrame(.Size4KiB, frame);
+    }
+
+    pub fn deallocate2MiB(frameAllocator: *FrameAllocator, frame: paging.PhysFrame2MiB) callconv(.Inline) void {
+        return frameAllocator.deallocateFrame(.Size2MiB, frame);
+    }
+
+    pub fn deallocate1GiB(frameAllocator: *FrameAllocator, frame: paging.PhysFrame1GiB) callconv(.Inline) void {
+        return frameAllocator.deallocateFrame(.Size1GiB, frame);
+    }
+
     /// Allocate a frame of the appropriate size and return it if possible.
     pub fn allocateFrame(frameAllocator: *FrameAllocator, comptime size: paging.PageSize) callconv(.Inline) ?paging.CreatePhysFrame(size) {
         return switch (size) {
