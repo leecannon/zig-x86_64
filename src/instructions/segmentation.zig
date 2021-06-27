dad54a29e882089e291780e9978b99f968d7fb60
Lee Cannon <leecannon@leecannon.xyz>
2021-06-27 18:41:02 +0100

    Update from upstream

--- src/instructions/segmentation.zig
+++ src/instructions/segmentation.zig
@@ -1,11 +1,24 @@
 usingnamespace @import("../common.zig");
 
+/// Returns the current value of the code segment register.
+pub fn getCs() x86_64.structures.gdt.SegmentSelector {
+    return .{
+        .value = asm ("mov %%cs, %[ret]"
+            : [ret] "=r" (-> u16)
+        ),
+    };
+}
+
 /// Reload code segment register.
 ///
-/// Note this is special since we can not directly move
-/// to %cs. Instead we push the new segment selector
-/// and return value on the stack and use lretq
-/// to reload cs and continue at 1:.
+/// The segment base and limit are unused in 64-bit mode. Only the L (long), D
+/// (default operation size), and DPL (descriptor privilege-level) fields of the
+/// descriptor are recognized. So changing the segment register can be used to
+/// change privilege level or enable/disable long mode.
+///
+/// Note this is special since we cannot directly move to [`CS`]. Instead we
+/// push the new segment selector and return value on the stack and use
+/// `retfq` to reload [`CS`] and continue at the end of our function.
 pub fn setCs(sel: x86_64.structures.gdt.SegmentSelector) void {
     asm volatile ("pushq %[sel]; leaq 1f(%%rip), %%rax; pushq %%rax; lretq; 1:"
         :
@@ -14,8 +27,27 @@ pub fn setCs(sel: x86_64.structures.gdt.SegmentSelector) void {
     );
 }
 
+/// Returns the current value of the stack segment register.
+pub fn getSs() x86_64.structures.gdt.SegmentSelector {
+    return .{
+        .value = asm ("mov %%ss, %[ret]"
+            : [ret] "=r" (-> u16)
+        ),
+    };
+}
+
 /// Reload stack segment register.
-pub fn loadSs(sel: x86_64.structures.gdt.SegmentSelector) void {
+///
+/// Entirely unused in 64-bit mode; setting the segment register does nothing.
+/// However, in ring 3, the SS register still has to point to a valid
+/// [`Descriptor`] (it cannot be zero). This means a user-mode read/write
+/// segment descriptor must be present in the GDT.
+///
+/// This register is also set by the `syscall`/`sysret` and
+/// `sysenter`/`sysexit` instructions (even on 64-bit transitions). This is to
+/// maintain symmetry with 32-bit transitions where setting SS actually will
+/// actually have an effect.
+pub fn setSs(sel: x86_64.structures.gdt.SegmentSelector) void {
     asm volatile ("movw %[sel], %%ss"
         :
         : [sel] "r" (sel.value)
@@ -23,8 +55,19 @@ pub fn loadSs(sel: x86_64.structures.gdt.SegmentSelector) void {
     );
 }
 
+/// Returns the current value of the data segment register.
+pub fn getDs() x86_64.structures.gdt.SegmentSelector {
+    return .{
+        .value = asm ("mov %%ds, %[ret]"
+            : [ret] "=r" (-> u16)
+        ),
+    };
+}
+
 /// Reload data segment register.
-pub fn loadDs(sel: x86_64.structures.gdt.SegmentSelector) void {
+///
+/// Entirely unused in 64-bit mode; setting the segment register does nothing.
+pub fn setDs(sel: x86_64.structures.gdt.SegmentSelector) void {
     asm volatile ("movw %[sel], %%ds"
         :
         : [sel] "r" (sel.value)
@@ -32,8 +75,19 @@ pub fn loadDs(sel: x86_64.structures.gdt.SegmentSelector) void {
     );
 }
 
+/// Returns the current value of the es segment register.
+pub fn getEs() x86_64.structures.gdt.SegmentSelector {
+    return .{
+        .value = asm ("mov %%es, %[ret]"
+            : [ret] "=r" (-> u16)
+        ),
+    };
+}
+
 /// Reload es segment register.
-pub fn loadEs(sel: x86_64.structures.gdt.SegmentSelector) void {
+///
+/// Entirely unused in 64-bit mode; setting the segment register does nothing.
+pub fn setEs(sel: x86_64.structures.gdt.SegmentSelector) void {
     asm volatile ("movw %[sel], %%es"
         :
         : [sel] "r" (sel.value)
@@ -41,8 +95,17 @@ pub fn loadEs(sel: x86_64.structures.gdt.SegmentSelector) void {
     );
 }
 
+/// Returns the current value of the fs segment register.
+pub fn getFs() x86_64.structures.gdt.SegmentSelector {
+    return .{
+        .value = asm ("mov %%fs, %[ret]"
+            : [ret] "=r" (-> u16)
+        ),
+    };
+}
+
 /// Reload fs segment register.
-pub fn loadFs(sel: x86_64.structures.gdt.SegmentSelector) void {
+pub fn setFs(sel: x86_64.structures.gdt.SegmentSelector) void {
     asm volatile ("movw %[sel], %%fs"
         :
         : [sel] "r" (sel.value)
@@ -50,8 +113,17 @@ pub fn loadFs(sel: x86_64.structures.gdt.SegmentSelector) void {
     );
 }
 
+/// Returns the current value of the gs segment register.
+pub fn getGs() x86_64.structures.gdt.SegmentSelector {
+    return .{
+        .value = asm ("mov %%gs, %[ret]"
+            : [ret] "=r" (-> u16)
+        ),
+    };
+}
+
 /// Reload gs segment register.
-pub fn loadGs(sel: x86_64.structures.gdt.SegmentSelector) void {
+pub fn setGs(sel: x86_64.structures.gdt.SegmentSelector) void {
     asm volatile ("movw %[sel], %%gs"
         :
         : [sel] "r" (sel.value)
@@ -64,65 +136,56 @@ pub fn swapGs() void {
     asm volatile ("swapgs" ::: "memory");
 }
 
-/// Returns the current value of the code segment register.
-pub fn getCs() x86_64.structures.gdt.SegmentSelector {
-    return .{
-        .value = asm ("mov %%cs, %[ret]"
-            : [ret] "=r" (-> u16)
-        ),
-    };
+/// Reads the fs segment base address
+///
+/// ## Exceptions
+///
+/// If `CR4.fsgsbase` is not set, this instruction will throw an `#UD`.
+pub fn readFsBase() u64 {
+    return asm ("rdfsbase %[ret]"
+        : [ret] "=r" (-> u64)
+    );
 }
 
-/// Writes the FS segment base address
+/// Writes the fs segment base address
 ///
-/// ## Safety
+/// ## Exceptions
 ///
 /// If `CR4.fsgsbase` is not set, this instruction will throw an `#UD`.
 ///
 /// The caller must ensure that this write operation has no unsafe side
-/// effects, as the FS segment base address is often used for thread
+/// effects, as the fs segment base address is often used for thread
 /// local storage.
-pub fn wrfsbase(value: u64) void {
+pub fn writeFsBase(value: u64) void {
     asm volatile ("wrfsbase %[val]"
         :
         : [val] "r" (value)
     );
 }
 
-/// Reads the FS segment base address
+/// Reads the gs segment base address
 ///
-/// ## Safety
+/// ## Exceptions
 ///
 /// If `CR4.fsgsbase` is not set, this instruction will throw an `#UD`.
-pub fn rdfsbase() u64 {
-    return asm ("rdfsbase %[ret]"
+pub fn readGsBase() u64 {
+    return asm ("rdgsbase %[ret]"
         : [ret] "=r" (-> u64)
     );
 }
 
-/// Writes the GS segment base address
+/// Writes the gs segment base address
 ///
-/// ## Safety
+/// ## Exceptions
 ///
 /// If `CR4.fsgsbase` is not set, this instruction will throw an `#UD`.
 ///
 /// The caller must ensure that this write operation has no unsafe side
-/// effects, as the GS segment base address might be in use.
-pub fn wrgsbase(value: u64) void {
+/// effects, as the gs segment base address might be in use.
+pub fn writeGsBase(value: u64) void {
     asm volatile ("wrgsbase %[val]"
         :
         : [val] "r" (value)
-    );
-}
-
-/// Reads the GS segment base address
-///
-/// ## Safety
-///
-/// If `CR4.fsgsbase` is not set, this instruction will throw an `#UD`.
-pub fn rdgsbase() u64 {
-    return asm ("rdgsbase %[ret]"
-        : [ret] "=r" (-> u64)
     );
 }
 
