85ebfb65bd2a5a12916f47a00075f11827d1abbc
Lee Cannon <leecannon@leecannon.xyz>
2022-05-06 21:09:10 +0100

    Fix for zig master

--- src/structures/tss.zig
+++ src/structures/tss.zig
@@ -5,20 +5,21 @@ const std = @import("std");
 /// In 64-bit mode the TSS holds information that is not directly related to the task-switch mechanism,
 /// but is used for finding kernel level stack if interrupts arrive while in kernel mode.
 pub const TaskStateSegment = packed struct {
-    reserved_1: u32,
+    reserved_1: u32 = 0,
     /// The full 64-bit canonical forms of the stack pointers (RSP) for privilege levels 0-2.
-    privilege_stack_table: [3]x86_64.VirtAddr,
-    reserved_2: u64,
+    privilege_stack_table: [3]x86_64.VirtAddr = [_]x86_64.VirtAddr{x86_64.VirtAddr.zero()} ** 3,
+    reserved_2: u64 = 0,
     /// The full 64-bit canonical forms of the interrupt stack table (IST) pointers.
-    interrupt_stack_table: [7]x86_64.VirtAddr,
-    reserved_3: u64,
-    reserved_4: u16,
+    interrupt_stack_table: [7]x86_64.VirtAddr = [_]x86_64.VirtAddr{x86_64.VirtAddr.zero()} ** 7,
+    reserved_3: u64 = 0,
+    reserved_4: u16 = 0,
     /// The 16-bit offset to the I/O permission bit map from the 64-bit TSS base.
-    iomap_base: u16,
+    iomap_base: u16 = 0,
 
     /// Creates a new zeroed TSS
+    /// WARNING: Deprecated
     pub fn init() TaskStateSegment {
-        return @bitCast(TaskStateSegment, @as(std.meta.Int(.unsigned, @bitSizeOf(TaskStateSegment)), 0));
+        return .{};
     }
 
     test {
